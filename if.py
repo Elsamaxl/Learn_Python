94f2623c98924dd81ac8d20dea87e6d81cb5c68a
MJL <light354@gmail.com>
2015-08-27 19:57:43 +0800

    add if.py

---
+++ if.py
@@ -0,0 +1,10 @@
+#Filename:if.py
+number=23
+guess=int(raw_input('Enter a integer:'))
+if guess=number
+	print 'Congratulations,you guessed it.' #New block start here
+	print "(but you do not win any prizes!)" #New block end here
+else guess>number
+	print 'No,it is a little higher than that' #Another block
+else 	
+	
