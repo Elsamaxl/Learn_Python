12464b3661549de4151d7b095b65c7b8855e5830
MJL <light354@gmail.com>
2015-08-30 20:57:35 +0800

    fix if.py

--- if.py
+++ if.py
@@ -6,5 +6,10 @@ if guess=number
 	print "(but you do not win any prizes!)" #New block end here
 else guess>number
 	print 'No,it is a little higher than that' #Another block
-else 	
+else 
+	print 'No,it is a little lower than that'
+	#you must have guss>number to reach here
+print 'Done'
+#This last statement is always executed,after the if statement is executed
+
 	
