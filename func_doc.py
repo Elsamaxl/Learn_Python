98ebc00cdc9d4ef676900e03687b21f071eefef3
MJL <light354@gmail.com>
2017-06-09 21:10:19 +0800

    function

---
+++ func_doc.py
@@ -0,0 +1,15 @@
+#Filename:func_doc.py
+def printMax(x,y):
+	'''Print the maximum of two numbers.
+	
+	The two values must be integer.'''
+	x=int(x)
+	y=int(y)
+	if x>y:
+		print x,'is maximum.'
+	else :
+		print y,'is maximum.'
+
+printMax(3,5)
+print printMax.__doc__
+		
---
+++ func_return.py
@@ -0,0 +1,7 @@
+#Filename:func_return.py
+def maximum(x,y):
+	if x>y:
+		return x
+	else:
+		return y
+print maximum(4,9)
